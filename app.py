2c6cc6b2efdaf2c2faa0cb203e3443f088e57f76
Paul Micu <35611221+PaulMicu@users.noreply.github.com>
2021-03-31 22:45:41 -0400

    added some changes

--- app.py
+++ app.py
@@ -8,22 +8,24 @@ import numpy
 
 def app():
     goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    goal_state_scaled = '((1; 2; 3; 4); (5; 6; 7; 8); (9; 10; 11; 12); (13; 14; 15; 16))'
-    goal_state_scaled_more = '((1; 2; 3; 4; 5); (6; 7; 8; 9; 10); (11; 12; 13; 14; 15); (16; 17; 18; 19; 20); (21; 22; 23; 24; 25))'
-    
+    goal_state_scaled = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+    goal_state_scaled_little = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]
+    goal_state_scaled_little_more = [[1, 2, 3, 4, 5,6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24],  [25, 26, 27, 28, 29, 30],  [31, 32, 33, 34, 35, 36]]
+    goal_state_scaled_more = [[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14], [15, 16, 17, 18, 19, 20, 21], [22, 23, 24, 25, 26, 27, 28], [29, 30, 31, 32, 33, 34, 35], [36, 37, 38, 39, 40, 41, 42], [43, 44, 45, 46, 47, 48, 49]]
+
     try:
         input_file = open('input.txt', 'r')
         lines = input_file.readlines()
 
-        # dfs = DepthFirstSearch(goal_state, lines)
-        # itedeep = IterativeDeepening(goal_state, lines)
+        dfs = DepthFirstSearch(goal_state, lines)
+        itedeep = IterativeDeepening(goal_state, lines)
         a1 = AStar1(goal_state, lines)
         a2 = AStar2(goal_state, lines)
+        dfs.search()
+        itedeep.search()
         a1.search()
         a2.search()
-       
-      
-      
+        
     except FileNotFoundError:
         print("File does not exist!")
 
